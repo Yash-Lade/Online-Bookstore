b160e432925b7381e1fff10ccedb85d3321915b1
Yash Lade <yash.lade@softude.com>
2026-02-12 10:38:07 +0530

    Prepare for Vercel Deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+if DEBUG:
+    ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+else:
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 
